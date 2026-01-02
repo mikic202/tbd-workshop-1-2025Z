2031449602b46b70fa96806d469c932f129f1e17
mwewior <mikolaj.wewior@gmail.com>
2026-01-02 17:25:24 +0100

    fix: changed dataproc workers number from 2 to 4 and added variable | commented Kubernetes in main.tf

--- modules/dataproc/variables.tf
+++ modules/dataproc/variables.tf
@@ -24,3 +24,9 @@ variable "image_version" {
   type    = string
   default = "2.2.69-ubuntu22"
 }
+
+variable "num_workers" {
+  type        = number
+  description = "max number of workers"
+  default     = 4
+}
